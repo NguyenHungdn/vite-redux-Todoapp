9e1eb0944d5b2a11e4509dddcf3c229eb2b4052a
antialone <antialonedn1@gmail.com>
2023-08-17 07:19:36 +0700

    update addTodo

--- src/components/TodoList/temp.jsx
+++ src/components/TodoList/temp.jsx
@@ -1,16 +1,4 @@
 //REDUCER
-const initValue = { value: 0 };
-const rootReducer = (state = initValue, action) => {
-  switch (action.type) {
-    case 'INCREMENTtt':
-      return {
-        ...state,
-        value: state.value + 1,
-      };
-    default:
-      return state;
-    //
-  }
-};
-// ACTion
+
+// ACTION
 // ACTION create
--- src/redux/reducer.jsx
+++ src/redux/reducer.jsx
@@ -1,33 +1,39 @@
 const initState = {
-  filter: {
-    search: '', // giá trị input để filter
-    status: 'All', // trạng thái hiện tại của todo gồm All - Completed -To do
-    priority: [], //  độ ưu tiên
-  },
-  todoList: [
-    {
-      id: 1,
-      todo: 'Learn Yoga', // việc cần làm
-      completed: false, // đã hoàn thành hay chưa boolean
-      priority: 'Medium', // độ ưu tiên
-    },
-    { id: 2, todo: 'Learn Redux', completed: true, priority: 'Hight' },
-    { id: 3, todo: 'Learn JavaScript', completed: false, priority: 'Low' },
-  ],
+   filter: {
+      search: '', // giá trị input để filter
+      status: 'All', // trạng thái hiện tại của todo gồm All - Completed -To do
+      priority: [], //  độ ưu tiên
+   },
+   todoList: [
+      {
+         id: 1,
+         name: 'Learn Yoga', // việc cần làm
+         completed: false, // đã hoàn thành hay chưa boolean
+         priority: 'Medium', // độ ưu tiên
+      },
+      { id: 2, name: 'Learn Redux', completed: true, priority: 'High' },
+      { id: 3, name: 'Learn JavaScript', completed: false, priority: 'Low' },
+   ],
 };
 const rootReducer = (state = initState, action) => {
-  switch (action.type) {
-    case 'todoList/addTodo': {
-      return {
-        ...state,
-        todoList: [
-          ...state.todoList, //
-          { id: 5, todo: 'Learn Football', completed: false, priority: 'Low' },
-        ],
-      };
-    }
-    default:
-      return state;
-  }
+   switch (action.type) {
+      case 'todoList/addTodo': {
+         return {
+            ...state,
+            todoList: [
+               ...state.todoList, //
+               action.payload,
+            ],
+         };
+      }
+      case 'filters/searchFilterChange': {
+         return {
+            ...state,
+            filter: { ...state.filter, search: action.payload },
+         };
+      }
+      default:
+         return state;
+   }
 };
 export default rootReducer;
