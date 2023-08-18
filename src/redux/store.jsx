1fff61e8b7bad64fa9457be6ad5ed4cbe23c297e
antialone <antialonedn1@gmail.com>
2023-08-18 23:50:11 +0700

    update store with redux-tollkit

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,6 +1,17 @@
-import { createStore } from 'redux';
-import rootReducer from './reducer';
-import { composeWithDevTools } from 'redux-devtools-extension';
+// import { createStore } from 'redux';
+// import rootReducer from './reducer';
+// import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(rootReducer, composeWithDevTools());
+// const store = createStore(rootReducer, composeWithDevTools());
+// export default store;
+
+import { configureStore } from '@reduxjs/toolkit';
+import filtersReducer from '../components/Filters/filtersSlice';
+import todoListsReducer from '../components/TodoList/TodoSlice';
+const store = configureStore({
+   reducer: {
+      filters: filtersReducer,
+      todoList: todoListsReducer,
+   },
+});
 export default store;
